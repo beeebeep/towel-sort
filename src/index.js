7fc5ea70252c3c27398c68904923e4d83068aef6
beeebeep <YouCanWriteMe@ya.ru>
2021-03-08 23:50:55 +0700

    fix: implement algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+
+    return matrix
+        .map((item, index) => {
+            if (index % 2 !== 0) {
+                item.reverse();
+            }
+            return item;
+        })
+        .reduce((previousValue, item) => previousValue.concat(item), []);
+};
